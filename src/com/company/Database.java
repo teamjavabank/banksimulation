c4d47c3d7b9c36836bf33bc3373a08db1d706dd7
Jokke <jokke@oc3720325513.ibm.com>
2016-06-27 13:53:31 +0200

    add account to database

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -5,10 +5,8 @@ import java.util.*;
 public class Database {
 	
 	private Map<Integer, Customer> customerList = new HashMap<>();
-	private int cid;
-	private int aid;
-	/* private List<Account> accountList = new ArrayList<>();
-	private List <String> transactionList = new ArrayList<>(); */
+	private Map<Integer, Account> accountList = new HashMap<>();
+	private int cid, aid, tid;
 	
 	// Constructor
 	public Database() {
@@ -16,20 +14,43 @@ public class Database {
 		aid = 0;
 		tid = 0;
 	}
-	
-	
-	
+
+	/* --- Customer --- */
+
+	//setCustomer
 	public Customer setCustomer (Customer c) {
 		cid++;
 		return customerList.put(cid-1, c);
 	}
-	
+
+	//getCustomer
 	public Customer getCustomer (int customerId ) {
 		return customerList.get(customerId);
 	}
-	
+
+	//removeCustomer
 	public Customer removeCustomer (int customerId) {
 		return customerList.remove(customerId);
 	}
+
+	/* --- Account --- */
+
+	//setAccount
+	public Account setAccount(Account a) {
+		aid++;
+		return accountList.put(aid-1, a);
+	}
+
+	//getAccount
+	public Account getAccount (int accountId ) {
+		return customerList.get(accountId);
+	}
+
+	//removeAccount
+	public Account removeAccount (int accountId) {
+		return customerList.remove(accountId);
+	}
+
+	//Transaction
 	
 }
