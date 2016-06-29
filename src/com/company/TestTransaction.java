33830ac0889e6feb2d81b545d7b0431ca94f07f5
jokke150 <jokke@oc3720325513.ibm.com>
2016-06-29 13:18:33 +0200

    test transaction shitty

---
+++ src/com/company/TestTransaction.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class TestTransaction {
+
+	public static void main(String[] args) {
+		// initialize database
+		Database database = new Database();
+		Customer customer = new Customer();
+		Customer.addNewCustomer(customer, database);
+		Account
+
+		new Transaction(0, database);
+	}
+}
