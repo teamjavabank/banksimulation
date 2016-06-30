32a684801e6d1e3f79128ad72e5dcb325999cc25
Christoph <chadl@gmx.de>
2016-06-30 11:55:46 +0200

    Debugged, not working

--- src/com/company/Mortgage.java
+++ src/com/company/Mortgage.java
@@ -1,15 +1,18 @@
 package com.company;
 
+
 public class Mortgage extends Account {
 	public int id;
+	Database db;
 	
-	public Mortgage(String newAccountType, int newCustomerId, Database db){
-		super(newAccountType, newCustomerId, db);
-		this.setInterestRate(3.8);
+	public Mortgage(String newAccountType, int newCustomerId, Database db1){
+		super(newAccountType, newCustomerId, db1);
+		setInterestRate(3.8);
+		db = db1;
 	}
 	
-	public void setStartBalance(balance b){
-		this.subtractMoney(b, id,db);
+	public void setStartBalance(double b){
+		subtractMoney(b, id,db);
 	}
 
 }
--- src/com/company/MortgageApplication.java
+++ src/com/company/MortgageApplication.java
@@ -2,12 +2,21 @@ package com.company;
 
 import java.util.*;
 
+
 public class MortgageApplication {
 	
 	double amount;
+	String newAccountType;
+	int newCustomerId;
+	Database db;
 	
-	public MortgageApplication(String newAccountType, int newCustomerId, Database db){
-		rc = interview();
+	public MortgageApplication(String newAccountType1, int newCustomerId1, Database db1){
+		
+		newAccountType = newAccountType1;
+		newCustomerId = newCustomerId1;
+		db = db1;
+		
+		interview();
 		mortgageConditions();
 		makeMortage();
 	}
@@ -23,23 +32,24 @@ public class MortgageApplication {
 			System.out.println("How much money is the object worth?");
 			amount = (double) inputScanner.nextDouble();
 			if(Double.isNaN(amount)){
-				System.out.println("Please enter a valid number next time.")
+				System.out.println("Please enter a valid number next time.");
 			}else{
 				done = 1;
 			}
+		}
 	}
 	
-	private int mortageConditions(){
-		System.out.println("The Customer gets" + amount*0.8 + "€ and can pay back the mortgage for " + (amount*1.013*1,0735*0,8) + "€");
+	private void mortgageConditions(){
+		System.out.println("The Customer gets" + amount*0.8 + "€ and can pay back the mortgage for " + (amount*1.013*1.0735*0.8) + "€");
 		System.out.println(" ");
-		System.out.println("The Customer can get a mortgage of " + amount*0,8 +" € with a yearly interest of 3.8 percent for the first two years and an interest of 5 percent afterwards");
+		System.out.println("The Customer can get a mortgage of " + amount*0.8 +" € with a yearly interest of 3.8 percent for the first two years and an interest of 5 percent afterwards");
 		System.out.println("The mortgage will be paid off in 5 years, if: Yearly Rate:" + (amount*0.8*0.2*1.038*1.038*1.05*1.05*1.05)+ "€");
 		
 	}
 	
 	private void makeMortage(){
 	
-		Mortgage m = new Mortgage(String newAccountType, int newCustomerId, Database db);
+		Mortgage m = new Mortgage( newAccountType, newCustomerId, db);
 		int id = db.setAccount(m);
 		m.id = id;
 		m.setStartBalance(amount*0.8);
