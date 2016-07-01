05da487d2114e61822e8eabc992c343d3ad58483
Philipp Grosskreutz <philipp@grossx.de>
2016-07-01 11:26:23 +0200

    comments and println s

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -15,7 +15,10 @@ public class Customer {
 
 	private static Scanner inputScanner = new Scanner(System.in);
 
-
+	// -------------------------------------------------
+	// constructor
+	// -------------------------------------------------
+	
 	public Customer(Database db, String newName, String newAddress, String newPhone, String newSex, String newDob, String newPin)
 	{
 
@@ -30,7 +33,9 @@ public class Customer {
 		this.customerId = db.setCustomer(this);
 
 	}
-
+	// -------------------------------------------------
+	// add an account, individual specifications for the input values
+	// -------------------------------------------------
 	public Customer(Database db) {
 		String inputs;
 		boolean[] checklist = new boolean[6];
@@ -38,59 +43,59 @@ public class Customer {
 			checklist[c] = false;
 		}
 		while (checklist[0] == false) {
-			System.out.println("insert costumer name!");
+			System.out.println("Insert costumer name!");
 			inputs = (String) inputScanner.next();
 			if (inputs.length() >= 3 && inputs.length() < 25) {
 				this.name = inputs;
 				checklist[0] = true;
 			} else {
-				System.out.println("please enter a name between 3 and 25 characters");
+				System.out.println("Please enter a name between 3 and 25 characters.");
 			}
 		}
 
 		while (checklist[1] == false) {
-			System.out.println("insert costumer address!");
+			System.out.println("Insert costumer address!");
 			inputs = (String) inputScanner.next();
 			if (inputs.length() >= 5 && inputs.length() < 40) {
 				this.address = inputs;
 				checklist[1] = true;
 			} else {
-				System.out.println("please enter a name between 5 and 40 characters");
+				System.out.println("Please enter a name between 5 and 40 characters.");
 			}
 		}
 		while (checklist[2] == false) {
-			System.out.println("insert costumer phone number!");
+			System.out.println("Insert costumer phone number!");
 			inputs = (String) inputScanner.next();
 			if (inputs.length() >= 9 && inputs.length() < 21) {
 				this.phone = inputs;
 				checklist[2] = true;
 			} else {
-				System.out.println("please enter a number between 9 and 20 characters");
+				System.out.println("Please enter a number between 9 and 20 characters.");
 			}
 		}
 		while (checklist[3] == false) {
-			System.out.println("insert costumer gender!");
+			System.out.println("Insert costumer gender!");
 			inputs = (String) inputScanner.next();
-			if (inputs.equals("male") || inputs.equals("female")) {
+			if (inputs.equals("male") || inputs.equals("female") || inputs.equals("Male") || inputs.equals("Female")) {
 				this.sex = inputs;
 				checklist[3] = true;
 			} else {
-				System.out.println("please write if the costumer is a ``male`` or a ``female``");
+				System.out.println("Please write if the costumer is a ``male`` or a ``female``.");
 			}
 		}
 
-		System.out.println("insert the costumers date of birth! (DD/MM/YYYY)");
+		System.out.println("Insert the costumers date of birth! (DD/MM/YYYY)");
 		inputs = (String) inputScanner.next();
 		this.dob = inputs;
 
 		while (checklist[5] == false) {
-			System.out.println("insert a PIN!");
+			System.out.println("Insert a PIN!");
 			inputs = (String) inputScanner.next();
 			if (inputs.length() >= 4) {
 				this.pin = inputs;
 				checklist[5] = true;
 			} else {
-				System.out.println("the PIN has to be at least 4 characters long");
+				System.out.println("The PIN has to be at least 4 characters long.");
 			}
 		}
 
@@ -117,7 +122,7 @@ public class Customer {
 			for (int account: details.AccountList)
 			{
 
-				System.out.println("Account " + account + ": " + Account.getBalance(account, db) + " EUR");
+				System.out.println("Account " + account + ": " + Account.getBalance(account, db) + " EUR.");
 
 			}
 		}
@@ -149,75 +154,77 @@ public class Customer {
 			String input = inputScanner.next();
 			String newvalue = "";
 
+			// stops the updating process
 			if (input.equals("0")) {
 				update = false;
 				System.out.println("Updating stopped, changes are saved.");
+			// changes the customers name
 			} else if (input.equals("1")) {
-				System.out.println("insert new value!");
-				newvalue = (String) inputScanner.next();
-				newCustomer.name = newvalue;
-			} else if (input.equals("1")) {
-				System.out.println("insert new value!");
+				System.out.println("Insert new value!");
 				while (updatecheck[1] == false) {
 					newvalue = (String) inputScanner.next();
 					if (newvalue.length() >= 3 && newvalue.length() < 25) {
 						newCustomer.name = newvalue;
 						updatecheck[0] = true;
 					} else {
-						System.out.println("please enter a name between 3 and 25 charakters");
+						System.out.println("Please enter a name between 3 and 25 charakters.");
 					}
 				}
+			// changes the customers address
 			} else if (input.equals("2")) {
-				System.out.println("insert new value!!");
+				System.out.println("Insert new value!");
 				while (updatecheck[1] == false) {
 					newvalue = (String) inputScanner.next();
 					if (newvalue.length() >= 5 && newvalue.length() < 40) {
 						newCustomer.address = newvalue;
 						updatecheck[1] = true;
 					} else {
-						System.out.println("please enter a name between 5 and 40 charakters");
+						System.out.println("Please enter a name between 5 and 40 charakters.");
 					}
 				}
+			// changes the customers phonenumber	
 			} else if (input.equals("3")) {
-				System.out.println("insert new value!!");
+				System.out.println("Insert new value!");
 				while (updatecheck[2] == false) {
 					newvalue = (String) inputScanner.next();
 					if (newvalue.length() >= 9 && newvalue.length() < 20) {
 						newCustomer.phone = newvalue;
 						updatecheck[2] = true;
 					} else {
-						System.out.println("please enter a name between 9 and 20 charakters");
+						System.out.println("Please enter a name between 9 and 20 charakters.");
 					}
 				}
+			// changes the customers gender
 			} else if (input.equals("4")) {
-				System.out.println("insert new value!");
+				System.out.println("Insert new value!");
 				while (updatecheck[3] == false) {
 					newvalue = (String) inputScanner.next();
-					if (newvalue.equals("male") || newvalue.equals("female")) {
+					if (newvalue.equals("male") || newvalue.equals("female") || inputs.equals("Male") || inputs.equals("Female")) {
 						newCustomer.sex = newvalue;
 						updatecheck[3] = true;
 					} else {
 						System.out.println("please write if the costumer is a ``male`` or a ``female``");
 					}
 				}
+			// changes the customers pin, the old PIN is necessary for updating
 			} else if (input.equals("5")) {
-				System.out.println("insert new value!");
+				System.out.println("Insert new value!");
 				newvalue = (String) inputScanner.next();
 				newCustomer.dob = newvalue;
 			} else if (input.equals("6")) {
-				System.out.println("insert new pin!");
+				System.out.println("Insert new pin!");
 				while (updatecheck[5] == false) {
 					newvalue = (String) inputScanner.next();
 					if (newvalue.length() >= 0 && newvalue.length() < 5) {
 						updatecheck[5] = true;
 					} else {
-						System.out.println("please enter a number between 0 and 5 ");
+						System.out.println("Please enter a number between 0 and 5.");
 					}
 				}
-				System.out.println("repeat new pin!");
+				System.out.println("Repeat new pin!");
 				String newvalue2 = (String) inputScanner.next();				
 				if (newvalue.equals(newvalue2)) {
-					System.out.println("insert old pin!");
+					System.out.println("Insert old pin!");
 					newvalue = (String) inputScanner.next();
 					if ((oldCustomer.pin).equals(newvalue)) {
 						newCustomer.pin = newvalue2;
@@ -234,14 +241,18 @@ public class Customer {
 		}
 
 	}
-
+	// -------------------------------------------------
+	// removes a customer
+	// -------------------------------------------------
 	public static void removeCustomer (int customerId, Database d)
 	{
 
 		d.removeCustomer(customerId);
 
 	}
-
+	// -------------------------------------------------
+	// lists all accounts
+	// -------------------------------------------------
 	public static List<Integer> getAccounts (int customerId, Database d)
 	{
 
@@ -250,7 +261,9 @@ public class Customer {
 		return c.AccountList;
 
 	}
-
+	// -------------------------------------------------
+	// return the customers date of birth (is needed for junior account provement)
+	// -------------------------------------------------
 	public static String getDob (int customerId, Database d)
 	{
 
@@ -259,7 +272,9 @@ public class Customer {
 		return c.dob;
 
 	}
-
+	// -------------------------------------------------
+	// adds an account to a costumer
+	// -------------------------------------------------
 	public static void addAccount (int customerId, int accountId, Database d)
 	{
 
@@ -268,7 +283,9 @@ public class Customer {
 		c.AccountList.add(accountId);
 
 	}
-
+	// -------------------------------------------------
+	// removes an account of a costumer
+	// -------------------------------------------------
 	public static void removeAccount (int customerId, int accountId, Database d)
 	{
 
